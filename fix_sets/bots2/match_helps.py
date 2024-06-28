18061d7f9d55bff1f8944449bafe01acf7f4676b
ibrahem <ibrahem.al-radaei@outlook.com>
2024-06-29 00:07:58 +0300

    Update match_helps.py

--- fix_sets/bots2/match_helps.py
+++ fix_sets/bots2/match_helps.py
@@ -8,7 +8,7 @@ import re
 def match_id(content, title):
     # ---
     # * Image ID: [https://mdwiki.toolforge.org/images/1f1a1dd1cd3686f16594b05e17d72a.jpg 29774172]
-    ma2 = re.findall(r"Image ID: \[https://mdwiki.toolforge.org/images/[^[\]]+ (\d+)\]", content)
+    ma2 = re.findall(r"Image ID: \[[^[\]]+ (\d+)\]", content)
     # ---
     # match * Image ID: 10422592
     ma = re.findall(r"Image ID: (\d+)", content) or ma2
